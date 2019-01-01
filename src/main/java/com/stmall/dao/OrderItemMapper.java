613d0ad6df4f7291e6dc1fb28b2da7648ff6f542
ned <740472039@qq.com>
2019-01-01 23:10:31 +0800

    project init commit

---
+++ src/main/java/com/stmall/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.stmall.dao;
+
+import com.stmall.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
